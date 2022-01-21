dffe5515aee78cf83e571468b88dfb052fbb2d2b
Artem Titov <tematitov91@gmail.com>
2022-01-21 15:21:41 +0300

    burgerShop

--- src/components/burgerConstructors/burgerConstructors.tsx
+++ src/components/burgerConstructors/burgerConstructors.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ConstructorElement, DragIcon} from '@ya.praktikum/react-developer-burger-ui-components'
 import arr from '../utils/data';
 import burgerConstructor from './burgerConstructor.module.css';
+import PayOrder from '../payOrder/payOrder';
 
 const burgerPices = arr.map((el) => {
     if(el.type !== "bun") {
@@ -28,7 +29,7 @@ export const bun = burgerBun("60666c42cc7b410027a1a9b1");
 
 const BurgerConstructors = (props : any) => {
 const {bunObject} = props;
-    return (
+    return (<div>
       <div style={{ display: 'flex', flexDirection: 'column', gap: '15px' }} className={burgerConstructor.burgerConstructor}>
         <ConstructorElement
           type="top"
@@ -48,6 +49,8 @@ const {bunObject} = props;
           thumbnail={bunObject.image_mobile}
         />
       </div>
+      <PayOrder />
+      </div>
     )
   }
 
@@ -63,33 +66,3 @@ export default BurgerConstructors;
 
 
 
-
-// const burgerComponent = arr.map((el) => {
-
-//     return (
-//         <BurgerComponents {...el}
-//         key = {el._id}
-//         alt = {el.name}
-//         />
-//     )
-// })
-
-// const BurgerConstructor = (props:any) => {
-//     const { commonPrice } = props;
-//     return (
-//     <div className={'mt-10 ' + burgerConstructor.burgerConstructor}>
-//         <ul className = {burgerConstructor.burgerList}>
-//         { burgerComponent } 
-//         </ul>
-//         <div>
-//             <p>{ commonPrice }</p>
-//             <CurrencyIcon type="primary" />
-//             <Button type="primary" size="medium">
-//             Нажми на меня
-//             </Button>
-//         </div>
-//     </div>
-//     )
-// }
-
-// export default BurgerConstructor;
